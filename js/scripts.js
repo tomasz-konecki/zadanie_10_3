a78eb8cfaa784fd06dbbb0a59857c7792aff8349
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-11 15:46:07 +0200

    Done

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+    var carouselList = $("#carousel ul");
+
+    setInterval(changeSlide, 3000);
+
+    function changeSlide() {
+        carouselList.animate({
+            marginLeft: -400
+        }, 500, moveFirstSlide);
+    }
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first"),
+            lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({
+            marginLeft: 0
+        });
+    }
+});
